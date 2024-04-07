98041781d1a4b99cb0c2d961eb664a2a75f58a96
Renato <rena.f.oliveira@gmail.com>
2024-04-06 23:44:42 -0300

    testando o find all paged da camada de serviço

--- projeto-dscatalog/backend/dscatalog/src/test/java/br/com/bodegami/dscatalog/services/ProductServiceTest.java
+++ projeto-dscatalog/backend/dscatalog/src/test/java/br/com/bodegami/dscatalog/services/ProductServiceTest.java
@@ -1,17 +1,28 @@
 package br.com.bodegami.dscatalog.services;
 
+import br.com.bodegami.dscatalog.dto.ProductDTO;
+import br.com.bodegami.dscatalog.entities.Product;
 import br.com.bodegami.dscatalog.repositories.ProductRepository;
 import br.com.bodegami.dscatalog.services.exceptions.DatabaseException;
 import br.com.bodegami.dscatalog.services.exceptions.ResourceNotFoundException;
+import br.com.bodegami.dscatalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
@@ -26,7 +37,8 @@ class ProductServiceTest {
     private long existingId;
     private long nonExistingId;
     private long dependentId;
-
+    private PageImpl<Product> page;
+    private Product product;
 
 
     @BeforeEach
@@ -34,7 +46,31 @@ class ProductServiceTest {
         existingId = 1L;
         nonExistingId = 100L;
         dependentId = 3L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
 
+
+
+        when(productRepository.save(any())).thenReturn(product);
+
+        when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
+        when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+    }
+
+
+    @Test
+    public void findAllPagedShouldReturnPage() {
+
+        when(productRepository.findAll((Pageable) any())).thenReturn(page);
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<ProductDTO> result = service.findAllPaged(pageable);
+
+        assertNotNull(result);
+
+        verify(productRepository, times(1)).findAll((Pageable) any());
     }
 
     @Test
