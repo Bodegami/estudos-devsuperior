24792c28eac499d2e830384b1f9aca269b995d90
Renato <rena.f.oliveira@gmail.com>
2024-04-06 21:13:25 -0300

    testando save com id nulo

--- projeto-dscatalog/backend/dscatalog/src/test/java/br/com/bodegami/dscatalog/repositories/ProductRepositoryTest.java
+++ projeto-dscatalog/backend/dscatalog/src/test/java/br/com/bodegami/dscatalog/repositories/ProductRepositoryTest.java
@@ -1,6 +1,8 @@
 package br.com.bodegami.dscatalog.repositories;
 
 import br.com.bodegami.dscatalog.entities.Product;
+import br.com.bodegami.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,10 +17,31 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 100L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        Product result = productRepository.save(product);
+
+        assertNotNull(result.getId());
+        assertEquals(countTotalProducts + 1, product.getId());
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
         //ACT
-        long existingId = 1L;
 
         //ARRANGE
         productRepository.deleteById(existingId);
---
+++ projeto-dscatalog/backend/dscatalog/src/test/java/br/com/bodegami/dscatalog/tests/Factory.java
@@ -0,0 +1,23 @@
+package br.com.bodegami.dscatalog.tests;
+
+import br.com.bodegami.dscatalog.dto.ProductDTO;
+import br.com.bodegami.dscatalog.entities.Category;
+import br.com.bodegami.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product =  new Product(1L, "Phone", "Good Phone", 800.0,
+                "https://img.com/img.png", Instant.parse("2024-04-06T03:00:00Z"));
+        product.getCategories().add(new Category(2L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+
+}
