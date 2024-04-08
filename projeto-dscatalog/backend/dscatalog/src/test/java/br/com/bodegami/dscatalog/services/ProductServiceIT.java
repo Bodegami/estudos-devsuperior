2325124f9c80e4d42d4e22eafb1716df718379f2
Renato <rena.f.oliveira@gmail.com>
2024-04-07 21:11:53 -0300

    criando o primeiro teste de integracao

---
+++ projeto-dscatalog/backend/dscatalog/src/test/java/br/com/bodegami/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,48 @@
+package br.com.bodegami.dscatalog.services;
+
+import br.com.bodegami.dscatalog.repositories.ProductRepository;
+import br.com.bodegami.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private  ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() {
+
+        service.delete(existingId);
+
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+    }
+
+}
