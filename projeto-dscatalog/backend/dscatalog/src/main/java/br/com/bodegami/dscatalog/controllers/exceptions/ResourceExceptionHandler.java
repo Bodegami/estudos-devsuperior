20de8643ecbd8129e6469be0f1ce4b80d2d904a5
Renato <rena.f.oliveira@gmail.com>
2024-04-10 00:09:26 -0300

    custom validation response

--- projeto-dscatalog/backend/dscatalog/src/main/java/br/com/bodegami/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
+++ projeto-dscatalog/backend/dscatalog/src/main/java/br/com/bodegami/dscatalog/controllers/exceptions/ResourceExceptionHandler.java
@@ -5,13 +5,14 @@ import br.com.bodegami.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -26,10 +27,34 @@ public class ResourceExceptionHandler {
         return toErrorResponse(BAD_REQUEST, "Database Exception!", ex, request);
     }
 
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(UNPROCESSABLE_ENTITY.value());
+        err.setError("Validation Exception");
+        err.setMessage(ex.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for (FieldError f : ex.getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(UNPROCESSABLE_ENTITY).body(err);
+    }
+
     private ResponseEntity<StandardError> toErrorResponse(HttpStatus status, String errorMessage,
                                                           Exception ex, HttpServletRequest request) {
         StandardError errorResponse = new StandardError(
-                Instant.now(), status.value(), errorMessage, ex.getMessage(), request.getRequestURI());
+                Instant.now(),
+                status.value(),
+                errorMessage,
+                ex.getMessage(),
+                request.getRequestURI()
+
+
+
+        );
 
         return ResponseEntity.status(status).body(errorResponse);
     }
