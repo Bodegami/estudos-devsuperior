c14cba4fde352e2db9a465b3bf865115dd2e6223
Renato <rena.f.oliveira@gmail.com>
2024-04-11 21:16:35 -0300

    implementando o checklist do Spring Security PARTE 1

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,20 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+//public class DemoApplication implements CommandLineRunner {
 
-		boolean result = passwordEncoder.matches("123456", "$2a$10$VXUy89yz5qKoLCZYzoTnM.G9VxdTHn38ch74qWTNl.g0JLx4l5jRK");
-		System.out.println("MATCHES = " + result);
-	}
+//	@Autowired
+//	private PasswordEncoder passwordEncoder;
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+//
+//		boolean result = passwordEncoder.matches("123456", "$2a$10$VXUy89yz5qKoLCZYzoTnM.G9VxdTHn38ch74qWTNl.g0JLx4l5jRK");
+//		System.out.println("MATCHES = " + result);
+//	}
 }
