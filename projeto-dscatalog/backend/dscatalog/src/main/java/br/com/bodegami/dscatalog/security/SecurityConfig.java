92a70dbf539f08d7f51d384bf75c8d0e3451280e
Renato <rena.f.oliveira@gmail.com>
2024-04-14 11:30:07 -0300

    implementando o GrantedAuthority e o UserDetails

--- projeto-dscatalog/backend/dscatalog/src/main/java/br/com/bodegami/dscatalog/security/SecurityConfig.java
+++ projeto-dscatalog/backend/dscatalog/src/main/java/br/com/bodegami/dscatalog/security/SecurityConfig.java
@@ -1,28 +1,28 @@
-package br.com.bodegami.dscatalog.security;
+// Filtro utilizando antes de implementar o filtro do OAUTH2
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-                .securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
-                .build();
-
-
-    }
-
-}
+//package br.com.bodegami.dscatalog.security;
+//
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+//                .securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+//                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+//                .build();
+//    }
+//
+//}
